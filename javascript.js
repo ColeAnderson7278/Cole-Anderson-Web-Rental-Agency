9401218615a62d68a9afdb47c7da270ea547b524
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-10-12 11:17:46 -0500

    Added in the Rental Form
    I added in the form they will fill out once
    they've chosen what they'd like to rent.
    Once they click the checkout button it
    will replace the current HTML with the new
    HTML that displays the form.

--- javascript.js
+++ javascript.js
@@ -2,17 +2,96 @@ var source = document.querySelector("#listTemplate").innerHTML;
 var template = Handlebars.compile(source);
 
 function makingItemList() {
-    console.log(PAGE_DATA.items);
     for (item of PAGE_DATA.items) {
-        var html = template({
-            name: item.name,
-            price: item.price,
-            inStock: item.inStock
-        });
-        document
-            .querySelector("#itemListLocation")
-            .insertAdjacentHTML("beforeend", html);
+        if (item.name === "Car") {
+            var html = template({
+                name: item.name,
+                price: item.price,
+                inStock: item.inStock
+            });
+            document.querySelector("#carListLocation").innerHTML = html;
+        } else if (item.name === "Truck") {
+            var html = template({
+                name: item.name,
+                price: item.price,
+                inStock: item.inStock
+            });
+            document.querySelector("#truckListLocation").innerHTML = html;
+        } else if (item.name === "Motorcycle") {
+            var html = template({
+                name: item.name,
+                price: item.price,
+                inStock: item.inStock
+            });
+            document.querySelector("#motorcycleListLocation").innerHTML = html;
+        }
     }
 }
 
 makingItemList();
+
+// function carRentButton() {
+//     for (item of PAGE_DATA.items) {
+//         if (item.name === "Car" && item.inStock > -1) {
+//             var html = template({
+//                 name: item.name,
+//                 price: item.price,
+//                 inStock: (item.inStock -= 1)
+//             });
+//             document.querySelector("#carListLocation").innerHTML = html;
+//         } else {
+//             var html = template({
+//                 name: item.name,
+//                 price: item.price,
+//                 inStock: 0
+//             });
+//             document.querySelector("#carListLocation").innerHTML = html;
+//         }
+//     }
+// }
+
+// document
+//     .querySelector("#carListLocation")
+//     .addEventListener("click", carRentButton);
+
+function showForm() {
+    formTemplate = `<form class="ml-1">
+    <h3>Rental Form:</h3>
+    <div class="form-group">
+      <input type="text" class="form-control" placeholder="Name" required>
+    </div>
+    <div class="form-group">
+      <input type="text" class="form-control" placeholder="Address" required>
+    </div>
+    <div class="form-group">
+      <input type="phone" class="form-control" placeholder="Phone Number" required>
+    </div>
+    <div class="form-group">
+      <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email" required>
+    </div>
+    <div class="d-flex">
+        <div class="form-check mr-2">
+        <input class="form-check-input" type="radio" name="paymentType" value="option1" checked>
+        <label class="form-check-label">
+          Cash
+        </label>
+        </div>
+        <div class="form-check mr-2">
+          <input class="form-check-input" type="radio" name="paymentType" value="option2">
+          <label class="form-check-label">
+            Check
+          </label>
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="paymentType" value="option3">
+          <label class="form-check-label">
+            Card
+          </label>
+        </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+    </form>`;
+    document.querySelector("#purchasePage").innerHTML = formTemplate;
+}
+
+document.querySelector("#checkOutButton").addEventListener("click", showForm);
