2f6b133a416695a76767ac1c85c75ff38e5f51b9
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-10-19 13:46:40 -0500

    Made New Scripts
    I went in and turned my previous form into a handlebars script.
    I also added in page for after you check out that tells the user
    they are done, and gives them certain information.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,6 @@
-var source = document.getElementById("listTemplate").innerHTML;
-var template = Handlebars.compile(source);
-
 function makingItemList() {
+    var source = document.getElementById("listTemplate").innerHTML;
+    var template = Handlebars.compile(source);
     for (var item of PAGE_DATA.items) {
         var html = template(item);
         document
@@ -79,108 +78,14 @@ function turnOnCheckOut() {
 
 document.querySelector("body").addEventListener("click", turnOnCheckOut);
 
-function showForm() {
-    formTemplate = `<form onSubmit="submitMessage()" class="card m-1 p-2">
-    <div class="form-group">
-    <h4>Name:</h4>
-      <input type="text" class="form-control" placeholder="First Name" required>
-      <input type="text" class="form-control" placeholder="Last Name" required>
-    </div>
-    <div class="form-group">
-    <h4>Address:</h4>
-      <input type="text" class="form-control" placeholder="Street" required>
-      <input type="text" class="form-control" placeholder="City" required>
-      <select class="text-muted form-control" placeholder="State" required>
-        <option disabled selected>State</option>
-        <option>AL</option>
-        <option>AK</option>
-        <option>AZ</option>
-        <option>AR</option>
-        <option>CA</option>
-        <option>CO</option>
-        <option>CT</option>
-        <option>DE</option>
-        <option>FL</option>
-        <option>GA</option>
-        <option>HI</option>
-        <option>ID</option>
-        <option>IL</option>
-        <option>IN</option>
-        <option>IA</option>
-        <option>KS</option>
-        <option>KY</option>
-        <option>LA</option>
-        <option>ME</option>
-        <option>MD</option>
-        <option>MA</option>
-        <option>MI</option>
-        <option>MN</option>
-        <option>MS</option>
-        <option>MO</option>
-        <option>MT</option>
-        <option>NE</option>
-        <option>NE</option>
-        <option>NV</option>
-        <option>NH</option>
-        <option>NJ</option>
-        <option>NM</option>
-        <option>NY</option>
-        <option>NC</option>
-        <option>ND</option>
-        <option>OH</option>
-        <option>OK</option>
-        <option>OR</option>
-        <option>PA</option>
-        <option>RI</option>
-        <option>SC</option>
-        <option>SD</option>
-        <option>TN</option>
-        <option>TX</option>
-        <option>UT</option>
-        <option>VT</option>
-        <option>VA</option>
-        <option>WA</option>
-        <option>WV</option>
-        <option>WI</option>
-        <option>WY</option>
-      </select>
-      <input type="text" pattern="[0-9]{5}" class="form-control" placeholder="ZIP Code" required>
-      </div>
-    <div class="form-group">
-        <h4>Phone Number:</h4>
-      <input type="text" pattern="[0-9]{3} [0-9]{3} [0-9]{4}" class="form-control" placeholder="Example: 123 123 1234" required>
-    </div>
-    <div class="form-group">
-        <h4>Email:</h4>
-        <input type="email" class="form-control" placeholder="Example: myEmail@mail.com" required>
-    </div>
-    <div>
-        <h4>Choose Payment Type:</h4>
-        <div class="form-check ml-2">
-        <input class="form-check-input" type="radio" name="paymentType" value="option1" checked>
-        <label class="form-check-label">
-          Cash
-        </label>
-        </div>
-        <div class="form-check ml-2">
-          <input class="form-check-input" type="radio" name="paymentType" value="option2">
-          <label class="form-check-label">
-            Check
-          </label>
-        </div>
-        <div class="form-check ml-2">
-          <input class="form-check-input" type="radio" name="paymentType" value="option3">
-          <label class="form-check-label">
-            Card
-          </label>
-        </div>
-    </div>
-    <button type="submit" class="submitButton btn btn-success">Submit</button>
-    </form>`;
-    document.querySelector(".rentalContainer").innerHTML = formTemplate;
+function makeForm() {
+    var source = document.getElementById("formTemplate").innerHTML;
+    var template = Handlebars.compile(source);
+    var html = template({});
+    document.querySelector(".rentalContainer").innerHTML = html;
 }
 
-document.querySelector(".checkOutButton").addEventListener("click", showForm);
+document.querySelector(".checkOutButton").addEventListener("click", makeForm);
 
 function submitMessage() {
     var choice = confirm(
@@ -191,10 +96,23 @@ function submitMessage() {
         } per day will now be processed. Please confirm order.`
     );
     if (choice) {
-        alert("Thank you for your business. Please come again soon.");
+        makeThankYou();
     } else {
         alert(
             "Sorry if there was any problems with our services, but your order has been cancelled. Thanks for your time."
         );
     }
+}
+
+function makeThankYou() {
+    var firstName = document.querySelector(".firstName").value;
+    var lastName = document.querySelector(".lastName").value;
+    var source = document.getElementById("thankYouTemplate").innerHTML;
+    var template = Handlebars.compile(source);
+    var html = template({
+        greeting: `Thank you, ${firstName + " " + lastName}, for your business`,
+        orderStatus:
+            "Your order is now being processed, and you can be expecting an email containing your order number, the date purchased, and other information regarding your rental."
+    });
+    document.body.innerHTML = html;
 }
